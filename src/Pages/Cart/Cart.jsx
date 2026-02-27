9069814b62acc9c72bc7ea089b7c1a888ac92652
eyob42 <eyobadigeh07@gmail.com>
2026-02-27 04:27:37 +0300

    Update Cart functionality to calculate total based on item amounts; enhance reducer for basket item management

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -8,8 +8,9 @@ import { Link } from "react-router-dom";
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
   const total  = basket.reduce((amount, item)=>{
-    return item.price + amount
+    return item.price * item.amount + amount
   },0)
+  console.log(basket);
   
   return (
     <LayOut>
--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -7,15 +7,29 @@ export const initialState = {
 export const reducer = (state, action) => {
     switch (action.type) {
         case Type.ADD_TO_BASKET:
-            return {
-                ...state,
-                basket: [...state.basket, action.item]
+            // Check if the item exists
+            const existingItem = state.basket.find((item) => item.id === action.item.id);
+            
+            if (!existingItem) {
+                // Item doesn't exist - add new item
+                return {
+                    ...state,
+                    basket: [...state.basket, { ...action.item, amount: 1 }]
+                };
+            } else {
+                // Item exists - increase amount
+                const updatedBasket = state.basket.map((item) => {
+                    return item.id === action.item.id 
+                        ? { ...item, amount: item.amount + 1 } 
+                        : item;
+                });
+                return {
+                    ...state,
+                    basket: updatedBasket
+                };
             }
 
         default:
             return state;
-
     }
 }
-
-// const [state, dispatch] = useReducer(reducer, initialState)
