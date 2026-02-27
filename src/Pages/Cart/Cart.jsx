cd9dfe7ae283880340415222364848ffb471d260
eyob42 <eyobadigeh07@gmail.com>
2026-02-27 03:58:52 +0300

    Enhance Cart component with subtotal calculation and styling; add responsive layout

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -2,31 +2,50 @@ import React, { useContext } from "react";
 import Classes from "./Cart.module.css";
 import LayOut from "../../Components/LayOut/LayOut";
 import { DataContext } from "../../Components/DataProvider/DataProvider";
-import ProductCard from "../../Components/Product/ProductCard"
-
+import ProductCard from "../../Components/Product/ProductCard";
+import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
+import { Link } from "react-router-dom";
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
+  const total  = basket.reduce((amount, item)=>{
+    return item.price + amount
+  },0)
+  
   return (
     <LayOut>
-      <section>
-        <div>
+      <section className={Classes.container}>
+        <div className={Classes.cart__container}>
           <h2>Hello</h2>
-            <h3>Your shopping basket</h3>
-            <hr />
-            {
-              basket?.length===0?(<p>Opps! No item in your cart</p>):(
-                basket?.map((item, {i})=>{
-                  return <ProductCard 
+          <h3>Your shopping basket</h3>
+          <hr />
+          {basket?.length === 0 ? (
+            <p>Opps! No item in your cart</p>
+          ) : (
+            basket?.map((item, { i }) => {
+              return (
+                <ProductCard
                   key={i}
-                    product={item}
-                    renderDesc={true}
-                    renderAdd={false}
-                    flex={true}/>
-                })
-              )
-            }
+                  product={item}
+                  renderDesc={true}
+                  renderAdd={false}
+                  flex={true}
+                />
+              );
+            })
+          )}
         </div>
-        <div></div>
+        {basket?.length !== 0 && 
+        <div className={Classes.subtotal}>
+          <div>
+            <p>Subtotal ({basket?.length} items)</p>
+            <CurrencyFormat amount={total} />
+          </div>
+          <span>
+            <input type="checkbox" />
+            <small>This order contains a gift </small>
+          </span>
+          <Link To="/payments">Continue to checkout </Link>
+        </div>}
       </section>
     </LayOut>
   );
