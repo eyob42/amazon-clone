e6d1b4553cd1cf581f57d353c17eff4e6821e11d
eyob42 <eyobadigeh07@gmail.com>
2026-03-07 11:44:04 +0300

    Fix import case sensitivity for Product component

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
 import Category from '../../Components/Category/Category';
 import Carousel from '../../Components/Carousel/Carousel';
-import Product from '../../Components/Product/product';
+import Product from '../../Components/Product/Product';
 
 function Landing() {
   return (
