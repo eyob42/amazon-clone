b6b31fc2b8322d1e9b92645ea498ae6cd175b995
eyob42 <eyobadigeh07@gmail.com>
2026-03-07 11:22:01 +0300

    feat: update API endpoint URLs for production environment; uncomment BASE_URL for deployment

--- src/Api/endPoints.js
+++ src/Api/endPoints.js
@@ -1,7 +1,8 @@
 
 export const productUrl = "https://fakestoreapi.com";
-export const BASE_URL = 'http://localhost:5000/api';
-export const PRODUCTS_URL = `${BASE_URL}/products`;
-export const USERS_URL = `${BASE_URL}/users`;
-export const CART_URL = `${BASE_URL}/cart`;
-export const ORDERS_URL = `${BASE_URL}/orders`;
+// export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = 'https://amazon-api-ld36.onrender.com';
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;  // Your products
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CART_URL = `${BASE_URL}/api/cart`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
